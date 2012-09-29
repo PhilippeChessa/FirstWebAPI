873c56f2dcc2773ba14628588a45fb6ca6512495
Hinault <rdonfack@yahoo.fr>
2012-09-29 12:06:17 +0100

    First commit
    Upload files

---
+++ FirstWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstWebApiDAL;
+
+namespace FirstWebAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        Customer[] Customers = new Customer[] 
+        { 
+            new Customer { Id = 1, FirstName = "Hinault", LastName = "Romaric", EMail = "hr@gmail.com"}, 
+            new Customer { Id = 2, FirstName = "Thomas", LastName = "Perrin", EMail = "thomas@outlook.com"}, 
+            new Customer { Id = 3, FirstName = "Allan", LastName = "Croft", EMail = "allan.croft@crt.com"},
+            new Customer { Id = 3, FirstName = "Sahra", LastName = "Parker", EMail = "sahra@yahoo.com"},
+        };
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return Customers;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            var Customer = Customers.FirstOrDefault((c) => c.Id == id);
+            if (Customer == null)
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+            }
+            return Customer;
+        }
+
+        
+    }
+}
---
+++ TestFirstWebApi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using FirstWebApiDAL;
+
+
+namespace TestFirstWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.GetAsync("http://localhost:59130/api/customer").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var Customers = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+                foreach (var c in Customers)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}", c.FirstName, c.LastName, c.EMail);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            Console.ReadLine();
+        }
+    }
+}
